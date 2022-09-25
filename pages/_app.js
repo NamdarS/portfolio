b896164cf7c859617d62d8e53d3433da5c525910
Namdar S <namdarsadiq@gmail.com>
2022-09-25 19:17:59 -0400

    desk force

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="viewport" content="width=1920"/>
       </Head>
       <Component {...pageProps} />
     </>
