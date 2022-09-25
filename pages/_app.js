5279aa7be5474ddb2ff888c345bc16e73b7e4eda
Namdar S <namdarsadiq@gmail.com>
2022-09-25 19:19:20 -0400

    desk test

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import '../styles/globals.css'
-import Head from 'next/head'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=1920"/>
+        <meta name="viewport" content="width=1024" />
       </Head>
       <Component {...pageProps} />
     </>
   );
 }
 
-export default MyApp
+export default MyApp;
